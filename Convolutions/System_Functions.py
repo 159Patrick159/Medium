4612cdc2a4848d4a1390eb403e9acdfd8af591c6
Patrick Sandoval <patricksandovalromero@gmail.com>
2024-12-30 11:46:56 -0500

    Finished the notebooks for introduction to 1-D convlutions with basic system functions and introduction to convolution theorem. Downloaded temperature data to demonstrate filtering techniques.

--- Convolutions/System_Functions.py
+++ Convolutions/System_Functions.py
@@ -8,6 +8,8 @@
 # signal processing tasks.             #
 ########################################
 import numpy as np
+from scipy.signal import butter, cheby1, cheby2, bessel, freqresp
+from scipy.special import jv
 
 def boxcar(t,T):
     '''
@@ -51,3 +53,79 @@ def Hann(t,T):
         else:
             H.append(0)
     return(np.array(H))
+
+def MovingAverage(boxsize):
+    '''
+    ######################################################
+    Description: Return boxcar filter where amplitude
+    is defined as 1/N where N is the length of the 
+    boxcar
+    ######################################################
+    Inputs: 
+    t: boxsize
+    ######################################################
+    Outputs:
+    w: Boxcar array where each value is 1/N where N is
+    the length of the boxcar   
+    ######################################################
+    '''
+    return np.ones(boxsize)/boxsize
+    
+
+def sinc_function(f,k):
+    '''
+    ######################################################
+    Description: Returns normalized sinc function. Which 
+    is ideal for low pass filters with a flat frequency 
+    response in its passband.
+    ######################################################
+    Inputs:
+    f: frequency array
+    k: horizontal compression:
+    ######################################################
+    Outputs:
+    w: normalized sinc function of len f
+    ######################################################
+    '''
+    return np.sinc(f*k) # Numpy returns a normalized sinc funciton
+
+def ideal_boxcar(f,flow,fhigh):
+    '''
+    ######################################################
+    Description: Returns boxcar filter for specified
+    frequency range. This could be an idealized low-pass,
+    high-pass or band-pass filters.
+    ######################################################
+    Inputs:
+    f: frequency array
+    flow: lower bound for frequency filter
+    fhigh: upper bound for frequency filter
+    ######################################################
+    w: Boxcar filter for specified frequency range
+    ######################################################
+    '''
+    return np.where(np.logical_and(f <= fhigh, f>= flow),1,0)
+
+def bessel_function(f, n=0, omega=1):
+    '''
+    ######################################################
+    Description: Return bessel function for specified 
+    frequency array. Ideal for maximally flat group delay
+    ######################################################
+    Inputs: 
+    f: frequency array
+    n=
+
+    '''
+    return jv(n, omega * f)
+
+# 10. Chebyshev Polynomials (Transfer Function)
+def chebyshev1_filter(order, ripple, Wn):
+    return cheby1(order, ripple, Wn, output='sos')
+
+def chebyshev2_filter(order, attenuation, Wn):
+    return cheby2(order, attenuation, Wn, output='sos')
+
+# 11. Butterworth Response (Transfer Function)
+def butterworth_filter(order, Wn):
+    return butter(order, Wn, output='sos')
